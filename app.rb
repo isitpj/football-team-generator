79d05a2947125c00678292fca5115930b4fae3ba
Peter Johnstone <peter.wdj@gmail.com>
2017-11-22 23:36:09 +0000

    Add custom font from Google fonts and add hover pseudo-selector to #title element

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+ENV['RACK_ENV'] ||= 'development'
 require 'sinatra/base'
 
 class TeamGenerator < Sinatra::Base
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,8 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
+ENV['RACK_ENV'] = 'test'
+
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 require 'capybara'
