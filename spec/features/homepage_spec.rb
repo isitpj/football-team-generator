e74ef9e85fe4384d6fbdd6a48851463e41f4a44b
Peter Johnstone <peter.wdj@gmail.com>
2017-11-22 23:55:19 +0000

    Add passing test to create field to submit player names

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -3,4 +3,8 @@ feature 'Sets up the home page' do
     visit '/'
     expect(page).to have_text 'teamGen'
   end
+  scenario 'Renders a field to enter player names on the page' do
+    visit '/'
+    expect(page).to have_field 'players'
+  end
 end
