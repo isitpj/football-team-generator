dd6a937424d3d78fb51ae7105cf8612a03cae422
Peter Johnstone <peter.wdj@gmail.com>
2017-11-27 22:45:20 +0000

    DRY out code

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,18 +1,17 @@
 require './lib/game'
 
 describe Game do
+  game = Game.new
+
   it 'Should have players, initialised as an empty array' do
-    game = Game.new
     expect(game.players).to eq []
   end
 
   it 'Should have a team_one' do
-    game = Game.new
     expect(game.team_one).to eq []
   end
 
   it 'Should have a team_two' do
-    game = Game.new
     expect(game.team_two).to eq []
   end
 end
