d69aed7bdd1217d257bd697a9ae470e2cbef1de1
Peter Johnstone <peter.wdj@gmail.com>
2017-11-27 22:53:55 +0000

    Add accessor and creation class methods to Game

--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,14 @@ class Game
 
   attr_reader :players, :team_one, :team_two
 
+  def self.create(players = [])
+    @game = Game.new(players)
+  end
+
+  def self.instance
+    @game
+  end
+
   def initialize(players = [])
     @players = players
     @team_one = []
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,7 @@
 require './lib/game'
 
 describe Game do
-  game = Game.new
+  game = Game.create
 
   it 'Should have players, initialised as an empty array' do
     expect(game.players).to eq []
@@ -13,5 +13,18 @@ describe Game do
 
   it 'Should have a team_two' do
     expect(game.team_two).to eq []
+  end
+
+  describe '.create' do
+    it 'Should create a new instance of Game' do
+      expect(Game.create).to be_an_instance_of Game
+    end
+  end
+
+  describe '.instance' do
+    it 'Should return an instance of itself' do
+      game = Game.create
+      expect(Game.instance).to eq game
+    end
   end
 end
