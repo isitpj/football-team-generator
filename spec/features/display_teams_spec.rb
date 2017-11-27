49f16aeb00a148b0bac8b1b3dd63a1a7c908f704
Peter Johnstone <peter.wdj@gmail.com>
2017-11-27 22:30:01 +0000

    Change formatting and remove comments

--- spec/features/display_teams_spec.rb
+++ spec/features/display_teams_spec.rb
@@ -4,4 +4,11 @@ feature 'Displays the two teams' do
     expect(page).to have_content 'Team One'
     expect(page).to have_content 'Team Two'
   end
+
+  scenario 'Page displays a list of two random teams' do
+    visit '/'
+    fill_in 'players', with: 'Peter, Ed, Mark, Ainsley, Alex, Max, Sam, Jamie, Umair, Ignacio'
+    click_button 'generate'
+    expect('Peter, Ed, Ignacio, Alex, Umair').to appear_before('Team Two')
+  end
 end
