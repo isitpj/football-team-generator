3df5d6c14341198ea2b5208104f19b44bdfd341e
Peter Johnstone <peter.wdj@gmail.com>
2017-11-27 22:44:07 +0000

    Create variables to store two teams in Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,10 @@
 class Game
 
-  attr_reader :players
+  attr_reader :players, :team_one, :team_two
 
   def initialize(players = [])
     @players = players
+    @team_one = []
+    @team_two = []
   end
 end
