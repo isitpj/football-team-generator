37cd9506e32c44d6d906e4bb8aa6505efb03a25b
Peter Johnstone <peter.wdj@gmail.com>
2017-12-07 22:55:06 +0000

    Pass test to send user back to homepage by clicking change players button

--- spec/features/buttons_spec.rb
+++ spec/features/buttons_spec.rb
@@ -1,8 +1,18 @@
 feature 'Displays buttons giving the user action options' do
-  scenario 'Teams page has buttons to reshuffle teams or change players' do
+  before do
     Game.create
+  end
+
+  scenario 'Teams page has buttons to reshuffle teams or change players' do
     visit '/teams'
     expect(page).to have_button 'reshuffle teams'
     expect(page).to have_button 'change players'
+  end
+
+  scenario 'Clicking \'change players\' returns the user to the home page' do
+    visit '/teams'
+    click_link 'change players'
+    expect(page).to have_xpath('/')
+    expect(page).to have_content 'teamGen'
   end
 end
