b292e209235e122fcfceff4a76d53651be59f198
Peter Johnstone <peter.wdj@gmail.com>
2017-12-08 08:42:49 +0000

    Refactor logic to private method

--- lib/game.rb
+++ lib/game.rb
@@ -24,8 +24,7 @@ class Game
 
   def assign_teams
     shuffle_players
-    @team_one = []
-    @team_two = []
+    reset_teams
     @players.each do |player|
       @players.index(player) < (@players.length / 2) ? @team_one << player : @team_two << player
     end
@@ -35,5 +34,10 @@ class Game
 
   def shuffle_players
     @players.shuffle!
+  end
+
+  def reset_teams
+    @team_one = []
+    @team_two = []
   end
 end
