4f80acf5bb30151ff7e65f753103645fed28f6bb
Peter Johnstone <peter.wdj@gmail.com>
2017-12-08 08:58:22 +0000

    Isolate tests for buttons

--- spec/features/buttons_spec.rb
+++ spec/features/buttons_spec.rb
@@ -11,17 +11,21 @@ feature 'Displays buttons giving the user action options' do
 
   scenario 'Clicking \'change players\' returns the user to the home page' do
     visit '/teams'
-    click_link 'change players'
+    click_button 'change players'
     expect(page).to have_xpath('/')
     expect(page).to have_content 'teamGen'
   end
 
   scenario 'Clicking \'reshuffle teams\' reloads the page with newly reshuffled teams' do
+    player_ed = double("player_ed", name: 'Ed')
+    player_peter = double("player_peter", name: 'Peter')
+    player_ainsley = double("player_ainsley", name: 'Ainsley')
+    player_mark = double("player_mark", name: 'Mark')
     visit '/'
     fill_in 'players', with: 'Peter, Ed, Mark, Ainsley'
     click_button 'generate'
-    allow_any_instance_of(Game).to receive(:team_one).and_return([Player.new('Ed'), Player.new('Peter')])
-    allow_any_instance_of(Game).to receive(:team_two).and_return([Player.new('Ainsley'), Player.new('Mark')])
+    allow_any_instance_of(Game).to receive(:team_one).and_return([player_ed, player_peter])
+    allow_any_instance_of(Game).to receive(:team_two).and_return([player_ainsley, player_mark])
     click_button 'reshuffle teams'
     expect('Ed').to appear_before('Team Two')
     expect('Peter').to appear_before('Team Two')
